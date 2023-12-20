dfebfd6fff5e2f744726d12269504ea2c1c4e280
KesiaDD <145691282+KesiaDD@users.noreply.github.com>
2023-12-20 12:16:54 +0100

    test

--- kmers.py
+++ kmers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 def kmer2str(val, k):
     """ Transform a kmer integer into a its string representation
     :param int val: An integer representation of a kmer
@@ -9,32 +10,56 @@ def kmer2str(val, k):
     letters = ['A', 'C', 'T', 'G']
     str_val = []
     for i in range(k):
-        str_val.append(letters[val & 0b11])
+        str_val.append(letters[val & 0b11]) 
         val >>= 2
 
     str_val.reverse()
     return "".join(str_val)
 
-# def get_reverse(codon): 
-#     to_reverse = {"A": "T", "C":"G", "T":"A", "G":"C"}
-#     reverse = ''
-#     for ntd in codon: 
-#         reverse = reverse + to_reverse[ntd]
-#     return reverse[::-1]
+
+def get_reverse(codon): 
+    """
+    obtain the reverse codon
+    """
+    to_reverse = {"A": "T", "C":"G", "T":"A", "G":"C"}
+    reverse = ''
+    for ntd in codon:
+        if ntd not in to_reverse.keys():
+            break
+        else:
+            reverse = reverse + to_reverse[ntd]
+    return reverse[::-1]
 
 def stream_kmers(text, k):
+    """
+    on coupe le texte en mots de longueur k 
+    traduire ces mots ntd par ntd (de str à binaire)
+    on va faire un dictionnaire encodage
+    A = 0 --> en bit 00 
+    C = 1 --> en bit 01
+    T = 2 --> en bit 10
+    G = 3 --> en bit 11
+    """
     # --- To complete ---
-    liste_kmers = []
+    encodage = {"A":0, "C": 1, "T":2, "G":3}
+   
     for i in range(len(text)-k+1):
+        kmer_mask = 0 
         kmer = text[i:i+k]
-        # if kmer in liste_kmers: 
-        #     continue
-        # else: 
-        #     if get_reverse(kmer) not in liste_kmers: 
-        #         liste_kmers.append(kmer)
-        liste_kmers.append(kmer)
-    return liste_kmers
+        for letter in kmer: 
+            if letter not in encodage.keys(): 
+                break
+            else:
+                kmer_mask <<= 2 
+                kmer_mask |= encodage[letter]
 
+        reverse_kmer_mask = 0 
+        reverse_kmer = get_reverse(kmer)
+        for r_letter in reverse_kmer: 
+            reverse_kmer_mask <<= 2 
+            reverse_kmer_mask |= encodage[r_letter]
+            
+        yield kmer_mask, reverse_kmer_mask
 
 def main():
     pass
--- main.py
+++ main.py
@@ -3,32 +3,19 @@
 from loading import load_directory
 from kmers import stream_kmers, kmer2str
 import pandas as pd
-
+from collections import Counter
 
 def jaccard(fileA, fileB, k):
-    j = 0
-    # --- To complete ---
-    """
-    Prend en entrée les séquences ADN de 2 fichiers
-    et calcule la distance de Jaccard entre ces deux fichiers.
+    kmers_A = Counter(min(kmer) for seqA in fileA for kmer in stream_kmers(seqA, k))
+    kmers_B = Counter(min(kmer) for seqB in fileB for kmer in stream_kmers(seqB, k))
 
-    """
-    
-    commun = set()
-    kmers_A = stream_kmers(fileA[0],k)
-    kmers_B = stream_kmers(fileB[0],k)
-    # print("type(kmers a et B)", type(kmers_A), type(kmers_B))
-    # print(kmers_A[:10])
+    common_kmers = set(kmers_A.keys()) & set(kmers_B.keys())
+    common_elem = sum(min(kmers_A[kmer], kmers_B[kmer]) for kmer in common_kmers)
 
-    unique_kmers = set(kmers_A + kmers_B)
-    set_A = set(kmers_A)
-    set_B = set(kmers_B)
+    total = sum(kmers_A.values()) + sum(kmers_B.values()) - common_elem
 
-    if (set_A & set_B): 
-        commun = (set_A & set_B)
-    
-    j = len(commun)/len(unique_kmers)
-    # print(f"pour {fileA} et {fileB} on a {j}")
+    j = common_elem / total
+    print("j =", j)
 
     return j
 
@@ -36,21 +23,24 @@ def jaccard(fileA, fileB, k):
 
 if __name__ == "__main__":
     # Load all the files in a dictionary
+
+    # files = load_directory("datatoy")
+    # k = 5 
+
     files = load_directory("data")
     k = 21
     
     filenames = list(files.keys())
     Matrice_jaccard = pd.DataFrame(columns=filenames, index = filenames)
     for i in range(len(files)):
-        print(i,"-------------------------")
+        print(i, '--------------------------------')
         for j in range(i+1, len(files)):
-            print(j, "=========================")
-            
-            # --- Complete here ---
+            print(j, '=====================================')
+          
             j_index = jaccard(files[filenames[i]], files[filenames[j]], k)
             Matrice_jaccard[filenames[i]][filenames[j]] = j_index
-           
-            # print(filenames[i], filenames[j], j_index)
-print('la MATRICE')
-print(Matrice_jaccard)
-print(Matrice_jaccard.max().max())
+            print(filenames[i], filenames[j], j_index)
+    print('la MATRICE')
+    print(Matrice_jaccard)
+
+
